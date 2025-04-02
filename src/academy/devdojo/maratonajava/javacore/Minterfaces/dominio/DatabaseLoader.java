8a17b6626f7d5a834d77c9b7e9e1a4b9ee9aef82
Tiago Lohan <tiiagolohan@gmail.com>
2025-04-02 00:09:24 -0300

    Realizado aulas de interface.

---
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DatabaseLoader.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Minterfaces.dominio;
+
+public class DatabaseLoader implements DataLoader, DataRemover {
+
+    @Override
+    public void load() {
+        System.out.println("Carregando dados do banco de dados");
+    }
+
+
+    @Override
+    public void remove() {
+        System.out.println("Removendo dados do banco de dados");
+    }
+
+}
